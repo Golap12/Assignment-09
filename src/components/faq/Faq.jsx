8ef75f676573eba128049a60033fa0445aec8413
Golap12 <golaphasan379@gmail.com>
2024-04-13 10:44:34 +0600

    Testimonial and FAQ section added

---
+++ src/components/faq/Faq.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+
+const Faq = () => {
+    const [faq, setFaq] = useState([
+        {
+            question: 'How to schedule a property viewing?',
+            answer: 'To schedule a property viewing, simply browse our listings and select the property you are interested in. Then, click on the "Schedule Viewing" button and fill out the form with your preferred date and time. Our team will get back to you to confirm the appointment.',
+            open: false
+        },
+        {
+            question: 'What are the steps to submit an offer?',
+            answer: 'Submitting an offer is easy! Once you have found a property you love, click on the "Make an Offer" button on the property listing page. Fill out the required details and submit your offer. Our agents will review it and get in touch with you to discuss further.',
+            open: false
+        },
+        {
+            question: 'How can I request more information about a property?',
+            answer: 'If you need more information about a property, simply click on the "Request Info" button on the property listing page. Fill out the form with your questions or specific information you are seeking, and our team will promptly provide you with the details you need.',
+            open: false
+        },
+        {
+            question: 'What are the financing options available?',
+            answer: 'We offer a variety of financing options to suit your needs. Whether you are looking for a mortgage or other financing solutions, our team of experts is here to assist you. Simply reach out to us through our contact page, and we will guide you through the available options.',
+            open: false
+        }
+    ]);
+
+    const toggleFaq = (index) => {
+        setFaq(faq.map((item, i) => {
+            if (i === index) {
+                item.open = !item.open;
+            } else {
+                item.open = false;
+            }
+
+            return item;
+        }));
+    }
+
+    return (
+        <section className="py-10 bg-gray-50 sm:py-16 lg:py-24">
+            <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+                <div className="max-w-2xl mx-auto text-center">
+                    <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">Frequently Asked Questions</h2>
+                    <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-600">Explore some common questions below to get answers about our services.</p>
+                </div>
+
+                <div className="max-w-3xl mx-auto mt-8 space-y-4 md:mt-16">
+                    {faq.map((item, index) => (
+                        <div key={index} className="transition-all duration-200 bg-white border border-gray-200 cursor-pointer hover:bg-gray-50">
+                            <button type="button" className="flex items-center justify-between w-full px-4 py-5 sm:p-6" onClick={() => toggleFaq(index)}>
+                                <span className="flex text-lg font-semibold text-black"> {item.question} </span>
+
+                                <svg className={`w-6 h-6 text-gray-400 ${item.open ? 'rotate-180' : ''}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+                                </svg>
+                            </button>
+
+                            <div className={`${item.open ? 'block' : 'hidden'} px-4 pb-5 sm:px-6 sm:pb-6`}>
+                                <p dangerouslySetInnerHTML={{ __html: item.answer }}></p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+                <p className="text-center text-gray-600 textbase mt-9">Didn’t find the answer you are looking for? <a href="#" title="" className="font-medium text-blue-600 transition-all duration-200 hover:text-blue-700 focus:text-blue-700 hover:underline">Contact our support</a></p>
+            </div>
+        </section>
+    );
+}
+
+export default Faq;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable react/no-unescaped-entities */
 import AOS from "aos";
-import "aos/dist/aos.css"; // You can also use <link> for styles
-// ..
+import "aos/dist/aos.css";
 AOS.init();
 
-import { Link, useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import SwiperHero from "../swiper/SwiperHero";
 import Card from "../realEstateCard/Card";
 import { Helmet } from "react-helmet-async";
+import Testimonial from "../testimonial/Testimonial";
+import Faq from "../faq/Faq";
 
 const Home = () => {
   const allResidential = useLoaderData();
@@ -17,27 +18,37 @@ const Home = () => {
       <Helmet>
         <title>New Home-Home</title>
       </Helmet>
+
+      
       <div className="md:mt-10 mt-[0.5rem] relative">
         <SwiperHero></SwiperHero>
-        <div  className="absolute md:top-[100px] top-[50px] left-[10%] md:w-3/4 w-3/4  text-white">
-          <h1 className="text-4xl font-extrabold md:w-2/4">Welcome to Your Dream Home</h1>
+        <div className="absolute md:top-[100px] top-[50px] left-[10%] md:w-3/4 w-3/4  text-white">
+          <h1 className="text-4xl font-extrabold md:w-2/4">
+            Welcome to Your Dream Home
+          </h1>
           <p className="mt-5 text-[#ffffff8f]">
-          Find the perfect property for your lifestyle and create lasting memories. Whether you're seeking a cozy apartment in the heart of the city or a luxurious villa overlooking the ocean, we have something for everyone. Let us help you find your ideal home sweet home.
+            Find the perfect property for your lifestyle and create lasting
+            memories. Whether you're seeking a cozy apartment in the heart of
+            the city or a luxurious villa overlooking the ocean, we have
+            something for everyone. Let us help you find your ideal home sweet
+            home.
           </p>
-          <button className="px-4 py-2 bg-[#08ccc4] text-black font-medium text-lg mt-5">View Properties</button>
+          <button className="px-4 py-2 bg-[#08ccc4] text-black font-medium text-lg mt-5">
+            View Properties
+          </button>
         </div>
       </div>
 
       <div className=" rounded-lg py-10 md:mt-14">
-        <div data-aos="fade-down" data-aos-duration="1000" className="text-center ">
-          <h1
-            className="md:text-4xl text-2xl  font-bold"
-          >
+        <div
+          data-aos="fade-down"
+          data-aos-duration="1000"
+          className="text-center "
+        >
+          <h1 className="md:text-4xl text-2xl  font-bold">
             Our Choice Of Popular Residential
           </h1>
-          <p
-            className="my-3 text-[#0000008f] w-3/4 mx-auto"
-          >
+          <p className="my-3 text-[#0000008f] w-3/4 mx-auto">
             "Top Picks for Living" - Our curated selection highlights prime
             residential locales, emphasizing convenience, security, and quality
             amenities, ensuring an optimal living experience.
@@ -50,6 +61,17 @@ const Home = () => {
           ))}
         </div>
       </div>
+
+      <div>
+        <Testimonial></Testimonial>
+      </div>
+
+
+      <div>
+        <Faq></Faq>
+      </div>
+
+
     </div>
   );
 };
